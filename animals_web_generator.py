41145e05337124e4afce8d899292facbc4746d4f
Johannes Reim <reim.j82@gmail.com>
2026-05-09 12:12:24 +0200

    last polish

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,29 +7,38 @@ def load_data(file_path):
     return json.load(handle)
 
 
+def serialize_animal(animal_obj):
+    """Create a formatted string with the data of an animal for printing."""
+    output = ""
+    
+    name = animal_obj.get("name")
+    diet = animal_obj.get("characteristics", {}).get("diet")
+    animal_type = animal_obj.get("characteristics", {}).get("type")
+    locations = animal_obj.get("locations")[0]
+
+    animals_data_for_printing = {}
+    if name is not None:
+        animals_data_for_printing["Name"] = name
+    if diet is not None:
+        animals_data_for_printing["Diet"] = diet
+    if locations is not None:
+        animals_data_for_printing["Location"] = locations
+    if animal_type is not None:
+        animals_data_for_printing["Type"] = animal_type        
+    output += '<li class="cards__item">\n'
+    for datatype, datavalue in animals_data_for_printing.items():
+        output += (f"<strong>{datatype}</strong> : {datavalue}<br/>\n")
+    ##output += "<br/>"
+        
+    return output
+
+
 def print_animals_data(animals_data):
-    """Create a formatted string with the animal data for printing."""
+    """Create a formatted string with the data of all animals from animal_data for printing."""
     output = ""
     
     for animal in animals_data:
-        name = animal.get("name")
-        diet = animal.get("characteristics", {}).get("diet")
-        animal_type = animal.get("characteristics", {}).get("type")
-        locations = animal.get("locations")[0]
-
-        animals_data_for_printing = {}
-        if name is not None:
-            animals_data_for_printing["Name"] = name
-        if diet is not None:
-            animals_data_for_printing["Diet"] = diet
-        if locations is not None:
-            animals_data_for_printing["Location"] = locations
-        if animal_type is not None:
-            animals_data_for_printing["Type"] = animal_type        
-        output += '<li class="cards__item">\n'
-        for datatype, datavalue in animals_data_for_printing.items():
-            output += (f"<strong>{datatype}</strong> : {datavalue}<br/>\n")
-        output += "<br/>\n"
+        output += serialize_animal(animal)
         
     return output
         
@@ -47,10 +56,10 @@ def write_html_file(html_file, file_name):
         file.write(html_file)   
 
 
-def replace_placeholder_in_html(html_file, placeholder, data):
-    """Replace the placeholder with the data in a .html file.
+def replace_placeholder_in_template(template_html_file, placeholder, data_string):
+    """Replace the placeholder in a template .html file with the data_string.
     Writes the new .html file to directory."""
-    new_html_file = html_file.replace(placeholder, data)
+    new_html_file = template_html_file.replace(placeholder, data_string)
     write_html_file(new_html_file, "animals.html") 
 
 
@@ -58,7 +67,7 @@ def main():
     animals_data = load_data('animals_data.json')
     animal_data_for_printing = print_animals_data(animals_data)
     html_file = get_html_template("animals_template.html")
-    replace_placeholder_in_html(html_file, "__REPLACE_ANIMALS_INFO__",
+    replace_placeholder_in_template(html_file, "__REPLACE_ANIMALS_INFO__",
      animal_data_for_printing)
    
     
