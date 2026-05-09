cf2ee647e8255fef16be51c0ae1adf328551235e
Johannes Reim <reim.j82@gmail.com>
2026-05-09 11:36:44 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,7 +8,9 @@ def load_data(file_path):
 
 
 def print_animals_data(animals_data):
-    """Print the animal data in a formatted manner."""
+    """Create a formatted string with the animal data for printing."""
+    output = ""
+    
     for animal in animals_data:
         name = animal.get("name")
         diet = animal.get("characteristics", {}).get("diet")
@@ -25,14 +27,41 @@ def print_animals_data(animals_data):
         if animal_type is not None:
             animals_data_for_printing["Type"] = animal_type
         width = max(len(datatype) for datatype in animals_data_for_printing)
+        
+
         for datatype, datavalue in animals_data_for_printing.items():
-            print(f"{datatype:<{width}} : {datavalue}")
-        print()
+            output += (f"{datatype:<{width}} : {datavalue}\n")
+        output += "\n"
+        
+    return output
+        
+        
+def get_html_template(html_file):
+    """Load a .html file as a template."""
+    with open(html_file, "r") as file:
+        html_template = file.read()
+    return html_template
+
+
+def write_html_file(html_file, file_name):
+    """Write a .html file with the chosen filename to directory."""
+    with open(file_name, "w") as file:
+        file.write(html_file)   
+
+
+def replace_placeholder_in_html(html_file, placeholder, data):
+    """Replace the placeholder with the data in a .html file.
+    Writes the new .html file to directory."""
+    new_html_file = html_file.replace(placeholder, data)
+    write_html_file(new_html_file, "animal_data.html") 
 
 
 def main():
     animals_data = load_data('animals_data.json')
-    print_animals_data(animals_data)
+    animal_data_for_printing = print_animals_data(animals_data)
+    html_file =  get_html_template("animals_template.html")
+    replace_placeholder_in_html(html_file, "__REPLACE_ANIMALS_INFO__",
+     animal_data_for_printing)
    
     
 if __name__ == "__main__":
